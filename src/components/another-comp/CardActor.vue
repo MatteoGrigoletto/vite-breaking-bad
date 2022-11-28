cc3384f8a6e8e99bc2c1a2b593f8061f9e4c9ce7
MatteoGrigoletto <mttgrigoletto@gmail.com>
2022-11-28 16:47:57 +0100

    Aggiunge API con ciclo v-for

---
+++ src/components/another-comp/CardActor.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="card">
+        <div>
+            <img src="" alt="">
+        </div>
+        <div>
+            <h3>{{item.name}}</h3>
+            <p>{{}}</p>
+            <p>Alive</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'CardActor',
+        props:{
+            item:Object,
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+.card{
+    width: calc(100% / 5 - 10px);
+    background-color: aqua;
+}
+</style>
--- src/components/another-comp/ListActors.vue
+++ src/components/another-comp/ListActors.vue
@@ -1,14 +1,34 @@
 <template>
     <div class="container-70">
         <div>
-            
+            <p> Found {{listActor.length}} characters</p>
+        </div>
+        <div class="container-cards">
+            <CardActor v-for="actor in listActor"
+             :item="actor" />
         </div>
     </div>
+
 </template>
 
 <script>
+import axios from 'axios';
+import CardActor from './CardActor.vue';
     export default {
         name:'ListActors',
+        data(){{
+            return{
+                listActor: [],
+            }
+        }},
+        components:{
+            CardActor,
+        },
+        created(){
+            axios.get("https://www.breakingbadapi.com/api/characters").then((response) => {
+                this.listActor = response.data;
+            });
+        }
     }
 </script>
 
@@ -16,6 +36,24 @@
 
 .container-70{
     background-color: var(--bg_main);
+    padding: 30px;
+    margin-top: 20px;
+
+    div{
+        
+        p{
+            background-color: var(--black);
+            color: white;
+            padding: 10px;
+        }
+    }
+    .container-cards{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        margin-top: 30px;
+
+    }
 }
 
 </style>
